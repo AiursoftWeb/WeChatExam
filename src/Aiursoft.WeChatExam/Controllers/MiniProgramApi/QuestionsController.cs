bb89bbb524a1d124d80053dec107b16271ab0811
zetian <zetian@aiursoft.com>
2026-02-07 18:09:58 +0800

    get a specified number of random questions

--- src/Aiursoft.WeChatExam/Controllers/MiniProgramApi/QuestionsController.cs
+++ src/Aiursoft.WeChatExam/Controllers/MiniProgramApi/QuestionsController.cs
@@ -25,6 +25,7 @@ public class QuestionsController : ControllerBase
     /// <param name="tagName">Tag 显示名称 (可选)</param>
     /// <param name="mtql">MTQL 查询表达式 (可选，优先级高于 tagName)。例如: `rock && not metal`</param>
     /// <param name="type">题目类型 (可选)</param>
+    /// <param name="size">随机获得题目的数量(可选)</param>
     /// <returns>题目列表，按创建时间倒序</returns>
     /// <response code="200">成功返回题目列表</response>
     /// <response code="404">指定的分类不存在</response>
@@ -32,7 +33,7 @@ public class QuestionsController : ControllerBase
     [ProducesResponseType(typeof(List<QuestionDto>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> GetQuestions([FromQuery] Guid? categoryId, [FromQuery] string? tagName, [FromQuery] string? mtql, [FromQuery] QuestionType? type)
+    public async Task<IActionResult> GetQuestions([FromQuery] Guid? categoryId, [FromQuery] string? tagName, [FromQuery] string? mtql, [FromQuery] QuestionType? type, [FromQuery] int? size)
     {
         // 验证分类是否存在
         if (categoryId.HasValue)
@@ -89,8 +90,16 @@ public class QuestionsController : ControllerBase
                 .Where(q => q.QuestionTags.Any(qt => qt.Tag.NormalizedName == normalizedTagName));
         }
 
+        if (size.HasValue)
+        {
+            query = query.OrderBy(q => Guid.NewGuid()).Take(size.Value);
+        }
+        else
+        {
+            query = query.OrderByDescending(q => q.CreationTime);
+        }
+
         var questions = await query
-            .OrderByDescending(q => q.CreationTime)
             .Select(q => new QuestionDto
             {
                 QuestionType = q.QuestionType,
